eb024c0ecbaab15bb735bf08809d8d4b41acfd60
KovalRomanK <romankoval10@gmail.com>
2020-06-06 05:56:53 -0400

    base profile route

--- backend/src/core/profile/delivery/http/profile.handler.ts
+++ backend/src/core/profile/delivery/http/profile.handler.ts
@@ -2,8 +2,9 @@ import { Handler, Router } from 'express';
 import * as express from 'express';
 
 import { asyncMiddleware } from '../../../../lib/middlewares/async';
-
 import * as domain from '../../../domain';
+
+import * as validation from './profile.validation';
 
 export class ProfileHandler {
   constructor(private service: domain.ProfileService) {}
@@ -11,32 +12,33 @@ export class ProfileHandler {
   router(): Router {
     const router = express.Router();
 
-    router.post('/', asyncMiddleware(this.create));
-    router.get('/:profileID', asyncMiddleware(this.get));
-    router.put('/:profileID', asyncMiddleware(this.update));
-    router.delete('/:profileID', asyncMiddleware(this.delete));
+    router.post('/', validation.create, asyncMiddleware(this.create));
+    router.get('/:profileID', validation.get, asyncMiddleware(this.get));
+    router.put('/:profileID', validation.update, asyncMiddleware(this.update));
+    router.delete('/:profileID', validation.remove, asyncMiddleware(this.remove));
 
     return router;
   }
 
-  create: Handler = async (req, res, next) => {
-    const id = await this.service.create(req.body);
-    res.json(id);
+  create: Handler = async (req, res) => {
+    const profile = await this.service.create(req.body);
+    res.json(profile);
   };
 
-  get: Handler = async (req, res, next) => {
+  get: Handler = async (req, res) => {
     const profile = await this.service.get(req.params.profileID);
+    delete profile.qr.uri;
     res.status(201).json(profile);
   };
 
-  update: Handler = async (req, res, next) => {
+  update: Handler = async (req, res) => {
     const profile: domain.Profile = { id: req.params.profileID, ...req.body };
     await this.service.update(profile);
     res.sendStatus(204);
   };
 
-  delete: Handler = async (req, res, next) => {
-    await this.service.delete(req.params.profileID);
+  remove: Handler = async (req, res) => {
+    await this.service.remove(req.params.profileID);
     res.sendStatus(204);
   };
 }
